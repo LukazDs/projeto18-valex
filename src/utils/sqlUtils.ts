cb49cba066331b5be8bcfa15c5d8fba9fe895d51
Lucas Duarte <lukazds2016@gmail.com>
2022-09-03 13:25:35 -0300

    Creating gitignore

--- src/utils/sqlUtils.ts
+++ src/utils/sqlUtils.ts
@@ -1,9 +1,10 @@
-export function mapObjectToUpdateQuery({ object: {}, offset = 1 }) {
+export function mapObjectToUpdateQuery({ object: {}, offset=1 }) {
 
-  const objectColumns: string = Object.keys(object)
+  const objectColumns: string = Object.keys(Object)
     .map((key, index) => `"${key}"=$${index + offset}`)
     .join(",");
-  const objectValues: string[] = Object.values(object);
+
+  const objectValues: string[] = Object.values(Object);
 
   return { objectColumns, objectValues };
 }
